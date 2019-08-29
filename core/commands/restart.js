5ccb0dfa9bff630d792c395ed2996bc1313cb22c
Bryson <ingoldbw281@gmail.com>
2019-08-29 19:58:42 -0400

    Split Destroy Command into Restart and Stop

---
+++ core/commands/restart.js
@@ -0,0 +1,25 @@
+'use strict';
+const Command = require('../modules/Command.js');
+const config = require('../config.json');
+
+module.exports = new Command({
+  name: 'restart',
+  level: 10,
+  plugin: 'owner',
+  help: {
+    short: 'Logs the bot off of Discord.',
+    long: 'Stops the bot, waits 10 seconds, and then restarts the bot.',
+    usage: `${config.prefix}restart`,
+    example: `${config.prefix}restart`
+  },
+  run: async function (bundle) {
+    const { message, controller } = bundle;
+
+    message.react(String.fromCharCode(0x2705)).catch();
+
+    await controller.destroyBot();
+
+    // Exit with 0 to signal that the process should restart
+    process.exit(0);
+  }
+});
--- core/commands/stop.js
+++ core/commands/stop.js
@@ -3,14 +3,14 @@ const Command = require('../modules/Command.js');
 const config = require('../config.json');
 
 module.exports = new Command({
-  name: 'destroy',
+  name: 'stop',
   level: 10,
   plugin: 'owner',
   help: {
     short: 'Logs the bot off of Discord.',
     long: 'Logs out, terminates the connection to Discord, and destroys the client.',
-    usage: `${config.prefix}destroy`,
-    example: `${config.prefix}destroy`
+    usage: `${config.prefix}stop`,
+    example: `${config.prefix}stop`
   },
   run: async function (bundle) {
     const { message, controller } = bundle;
@@ -18,5 +18,8 @@ module.exports = new Command({
     message.react(String.fromCharCode(0x2705)).catch();
 
     await controller.destroyBot();
+
+    // Exit with 0 to signal that the process should not restart
+    process.exit(1);
   }
 });
