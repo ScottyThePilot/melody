36343fe1abd2c2bfc285abc5b577d24a70381bae
Bryson <ingoldbw281@gmail.com>
2019-09-03 17:21:38 -0400

    Add link to repository in changelog command

--- core/commands/changelog.js
+++ core/commands/changelog.js
@@ -7,12 +7,16 @@ const config = require('../config.json');
 const contents = require('../changeloglatest.json');
 const embed = new RichEmbed();
 
+const extra = 'Read the full changelog [here](https://github.com/ScottyThePilot/melody_v3/blob/master/changelog.md)';
+const description = (contents.description ? contents.description + '\n' : '') + extra;
+
 embed.setTitle(contents.title);
-embed.setDescription(contents.description);
+embed.setDescription(description);
 embed.setColor([114, 137, 218]);
 [].forEach.call(contents.fields, function (field) {
   embed.addField(field.name, field.value);
 });
+
 
 module.exports = new Command({
   name: 'changelog',
