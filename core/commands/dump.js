683bde6d62ceca5121647826dd37005be8461b67
Bryson <ingoldbw281@gmail.com>
2019-10-30 18:23:53 -0400

    Dump Fix

--- core/commands/dump.js
+++ core/commands/dump.js
@@ -20,31 +20,37 @@ module.exports = new Command({
 
     if (message.author.id === config.ownerID && (args[0] || '').startsWith('activity')) {
       await message.author.send(`Core Bot Logs:`, {
-        file: new Attachment('./core/data/main.log', 'main.log')
-      }).catch((reason) => {
-        console.log(reason);
-        message.author.send('Unable to attach file.').catch(msgFailCatcher);
+        file: new Attachment(melody.logger.path)
+      }).catch((error) => {
+        console.log(error);
+        message.author.send(`Unable to attach file. \`Error: ${error.code}\``).catch(msgFailCatcher);
       });
     } else if (ownedGuilds.size < 1) {
       await message.author.send('There are no logs to dump as you do not own any servers.');
     } else if (ownedGuilds.size === 1) {
       const guild = ownedGuilds.first();
+      const manager = melody.guildManagers.get(guild.id);
+
       await message.author.send(`Message logs for ${guild.name}:`, {
-        file: new Attachment(`./core/data/${guild.id}/latest.log`, `guild${guild.id}.log`)
-      }).catch((reason) => {
-        console.log(reason);
-        message.author.send('Unable to attach file.').catch(msgFailCatcher);
+        file: new Attachment(manager.logger.path, `guild${guild.id}.log`)
+      }).catch((error) => {
+        console.log(error);
+        message.author.send(`Unable to attach file. \`Error: ${error.code}\``).catch(msgFailCatcher);
       });
+      
+      message.author.dmChannel.stopTyping();
     } else if (!args[0]) {
       await message.author.send('Because you own more than one guild, you must specify a server ID to dump logs for.').catch(msgFailCatcher);
     } else {
       if (ownedGuilds.has(args[0])) {
         const guild = ownedGuilds.get(args[0]);
+        const manager = melody.guildManagers.get(guild.id);
+
         await message.author.send(`Message logs for ${guild.name}:`, {
-          file: new Attachment(`./core/data/${guild.id}/latest.log`, `guild${guild.id}.log`)
-        }).catch((reason) => {
-          console.log(reason);
-          message.author.send('Unable to attach file.').catch(msgFailCatcher);
+          file: new Attachment(manager.logger.path, `guild${guild.id}.log`)
+        }).catch((error) => {
+          console.log(error);
+          message.author.send(`Unable to attach file. \`Error: ${error.code}\``).catch(msgFailCatcher);
         });
       } else {
         await message.author.send('I can\'t find that guild.').catch(msgFailCatcher);
